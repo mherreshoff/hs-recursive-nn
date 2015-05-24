bf986f7ab9fb32ec7084e87d19a8e1b7b9da9be1
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2015-05-24 15:35:18 -0700

    Got RNNs to compile.

--- RecursiveNeuralNet.hs
+++ RecursiveNeuralNet.hs
@@ -1,3 +1,9 @@
+module RecursiveNeuralNet (
+  evalNet
+  ) where
+
+import Data.Tree
+import qualified Data.Vector.Storable as V
 import Numeric.LinearAlgebra.Array
 import Numeric.LinearAlgebra.Array.Util
 import TreeUtil
@@ -6,13 +12,19 @@ type NodeType = Int;
 
 type NetParameters = NodeType -> Array Double;
 
--- TODO(mherreshoff): get this to work.
+elementwiseOp :: (Coord a, Coord b) => (a -> b) -> NArray i a -> NArray i b
+elementwiseOp f = mapArray (V.map f)
+
+
 -- NodeType has to map to an array of dimension one higher than the arity of the node.
--- evalNet :: NetParameters -> Tree NodeType -> Tree (Array Double)
--- evalNet params = evaluationTree (sigmoid.product.params) where
---   letters = ['a'..'z']
---   product :: Array Double -> [Array Double] -> Array Double
---   product arr vecs = (!"1") $ foldl (*) (arr!letters) $
---      zipWith (!) vecs $ map (:[]) letters
---   sigmoid :: Array Double -> Array Double
---   sigmoid = fmap (\x -> 1.0 / (1.0 + exp (0.0-x)))
+evalNet :: NetParameters -> Tree NodeType -> Tree (Array Double)
+evalNet params tree = evaluationTree eval tree where
+  eval :: NodeType -> [Array Double] -> Array Double
+  eval id children = elementwiseOp sigmoid $ product (params id) children
+  sigmoid :: Double -> Double
+  sigmoid x = 1.0 / (1.0 + exp (0.0-x))
+  product :: Array Double -> [Array Double] -> Array Double
+  product arr vecs = (!"1") $ foldl (*) (arr!letters) $
+     zipWith (!) vecs $ map (:[]) letters
+  letters :: [Char]
+  letters = ['a'..'z']
