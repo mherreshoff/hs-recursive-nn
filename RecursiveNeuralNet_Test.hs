c1eb08e92e72abf1a4b200f0db676eee15d5c207
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2015-05-24 16:07:05 -0700

    Wrote test for a 2-ary tensor.

--- RecursiveNeuralNet_Test.hs
+++ RecursiveNeuralNet_Test.hs
@@ -12,7 +12,9 @@ import Numeric.LinearAlgebra.Array.Util
 x = Node 0 []
 y = Node 1 []
 neg a = Node 2 [a]
+ewProd a b = Node 3 [a, b]
 
+indicator f = (\x -> if x then 1.0 else 0.0).f
 -- here's an example array definitions
 exampleParams :: NetParameters
 -- x and y:
@@ -20,19 +22,26 @@ exampleParams 0 = listArray [5] [1,-1,5,-6,3]
 exampleParams 1 = listArray [5] [3,-2,-6,1,-2]
 
 -- neg:
-exampleParams 2 = mkFun [5,5] (\[x,y] -> if x+y == 4 then 1.0 else 0.0)
+exampleParams 2 = mkFun [5,5] $ indicator (\[x,y] -> x+y == 4)
 
-exampleEval a = evalNet id exampleParams a
+-- ewProd:
+exampleParams 3 = mkFun [5,5,5] $ indicator (\[x,y,z] -> x == y && y == z)
+
+exampleEval a = rootLabel $ evalNet id exampleParams a
 
 -- Tests:
 prop_double_flip_is_id0 :: Bool
 
 prop_double_flip_is_id0 =
-  exampleEval x == exampleEval (neg (neg x))
+  exampleParams 0 == exampleEval (neg (neg x))
 
 prop_double_flip_is_id1 :: Bool
 prop_double_flip_is_id1 =
-  exampleEval y == exampleEval (neg (neg y))
+  exampleParams 1 == exampleEval (neg (neg y))
+
+prop_ew_prod_works :: Bool
+prop_ew_prod_works =
+  listArray [5] [3, 2, -30, -6, -6] == exampleEval (ewProd x y)
 
 -- return [] is TemplateHaskell magic to list the properties.
 return []
